76459cd0fdb4b6e0d54eeab399ca2f2084141fb3
root <root@DESKTOP-420V42T.localdomain>
2025-05-08 16:26:36 -0500

    gendiff arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,5 +6,12 @@ import { program } from "commander";
 program
     .description("Compares two configuration files and shows a difference.")
     .version('0.0.1')
+    .option('-f, --format <type>', 'output format')
+    .argument('<filepath1>', 'first file path')
+    .argument('<filepath2>', 'second file path')
+    .action((filepath1, filepath2) => {
+    console.log('filepath1:', filepath1);
+    console.log('filepath2:', filepath2);
+    })
     .parse();
 
