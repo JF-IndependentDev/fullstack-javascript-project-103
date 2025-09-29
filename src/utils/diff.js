dad1a19407ac55bbbeeebf37d87c1cc9d8da35fa
Juan Francisco C <juanfr.correal@gmail.com>
2025-09-29 11:35:31 -0500

    sep29

--- src/utils/diff.js
+++ src/utils/diff.js
@@ -1,16 +1,17 @@
 const getDiff = (obj1, obj2) => {
-  const keys = Array.from(new Set([...Object.keys(obj1), ...Object.keys(obj2)])).sort();
+  const keys = [...new Set([...Object.keys(obj1), ...Object.keys(obj2)])];
+  const sortedKeys = keys.slice().sort();
 
-  return keys.map((key) => {
-    if (!Object.hasOwn(obj1, key)) {
+  return sortedKeys.map((key) => {
+    if (!Object.prototype.hasOwnProperty.call(obj1, key)) {
       return { key, type: 'added', value: obj2[key] };
     }
-    if (!Object.hasOwn(obj2, key)) {
+    if (!Object.prototype.hasOwnProperty.call(obj2, key)) {
       return { key, type: 'removed', value: obj1[key] };
     }
     if (obj1[key] !== obj2[key]) {
       return {
-        key, type: 'changed', oldValue: obj1[key], newValue: obj2[key]
+        key, type: 'changed', oldValue: obj1[key], newValue: obj2[key],
       };
     }
     return { key, type: 'unchanged', value: obj1[key] };
